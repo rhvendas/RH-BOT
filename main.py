777c92ca9036656a08eb4c502103551d2228c4eb
rhvendas <rhuanigorlicurgomuch@gmail.com>
2025-01-10 13:09:54 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from rhvendas import rhvendas
+from cpmelsedev import CPMElsedev
 
 __CHANNEL_USERNAME__ = "rh_vendas_ofc"
 __GROUP_USERNAME__   = "5569993571857"
