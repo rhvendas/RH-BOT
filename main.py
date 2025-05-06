f8be41e60f64639419f3343081d9bd0a96076e4a
rhvendas <rhuanigorlicurgomuch@gmail.com>
2025-05-05 23:31:05 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -162,7 +162,7 @@ if __name__ == "__main__":
             load_player_data(cpm)
             load_key_data(cpm)
             load_client_details()
-            choices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"]
+            choices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]
             print(Colorate.Horizontal(Colors.rainbow, '{01}: ADICIONAR DINHEIRO           1.000K'))
             print(Colorate.Horizontal(Colors.rainbow, '{02}: ADICIONAR GOLDS              3.500K'))
             print(Colorate.Horizontal(Colors.rainbow, '{03}: INSERIR RANK KING            4.000K'))
@@ -193,6 +193,7 @@ if __name__ == "__main__":
             print(Colorate.Horizontal(Colors.rainbow, '{28}: BUSCAR INFORMACOES DO CARRO  5.000K'))
             print(Colorate.Horizontal(Colors.rainbow, '{29}: DEIXAR CARRO GLITH           3.500K'))
             print(Colorate.Horizontal(Colors.rainbow, '{30}: BUSCAR TODOS CARROS DA CONTA 5.000K'))
+            print(Colorate.Horizontal(Colors.rainbow, '{31}: BUSCAR TODOS CARROS DA CONTA FREE'))
             print(Colorate.Horizontal(Colors.rainbow, '{0} : SAIR'))
             
             print(Colorate.Horizontal(Colors.rainbow, '===============[ 𝐂𝐏𝐌☆ ]==============='))
