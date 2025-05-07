de0b28fce7f73a67179124c3d689495a7803c772
rhvendas <rhuanigorlicurgomuch@gmail.com>
2025-05-06 21:36:43 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -686,8 +686,7 @@ if __name__ == "__main__":
                 	print(Colorate.Horizontal(Colors.rainbow, 'FALHA.'))
                 	print(Colorate.Horizontal(Colors.rainbow, 'CARRO NÃO ENCONTRADO OU ERRO.'))
                 	sleep(2)
-                	continue
-                	else:
+                	else: continue
                 		print(Colorate.Horizontal(Colors.red_to_white, "ID INVÁLIDO. DIGITE UM NÚMERO."))
                 		sleep(2)
                 		continue
