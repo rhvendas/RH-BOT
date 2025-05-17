4884beb5de80d39fe08cedc5bccb4dfc9df94a13
rhvendas <rhuanigorlicurgomuch@gmail.com>
2025-05-17 17:56:19 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -162,7 +162,7 @@ if __name__ == "__main__":
             load_player_data(cpm)
             load_key_data(cpm)
             load_client_details()
-            choices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26"]
+            choices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27"]
             print(Colorate.Horizontal(Colors.rainbow, '{01}: ADICIONAR DINHEIRO           1.000K'))
             print(Colorate.Horizontal(Colors.rainbow, '{02}: ADICIONAR GOLDS              3.500K'))
             print(Colorate.Horizontal(Colors.rainbow, '{03}: INSERIR RANK KING            4.000K'))
@@ -189,6 +189,7 @@ if __name__ == "__main__":
             print(Colorate.Horizontal(Colors.rainbow, '{24}: ALTERAR CORRIDAS GANHAS      1.000K'))
             print(Colorate.Horizontal(Colors.rainbow, '{25}: ALTERAR CORRIDAS PERDIDAS    1.000K'))
             print(Colorate.Horizontal(Colors.rainbow, '{26}: CLONAR CONTA                 5.000K'))
+            print(Colorate.Horizontal(Colors.rainbow, '{26}: GERAR CONTA FULL             5.000K'))
             print(Colorate.Horizontal(Colors.rainbow, '{0} : SAIR'))
             
             print(Colorate.Horizontal(Colors.rainbow, '===============[ 𝐂𝐏𝐌☆ ]==============='))
@@ -608,29 +609,29 @@ if __name__ == "__main__":
                     print(Colorate.Horizontal(Colors.rainbow, '[!] USE VALORES VALIDOS.'))
                     sleep(2)
                     continue
-             elif service == 27: # Account Register VIP
-                 print(Colorate.Horizontal(Colors.rainbow, '[!] REGISTRANDO CONTA VIP COM 500K GOLDS.'))
-                 acc2_email = prompt_valid_value("[?] INSIRA UM EMAIL", "Email", password=False)
-                 acc2_password = prompt_valid_value("[?] INSIRA UMA SENHA", "Password", password=False)
-                 console.print("[%] CRIANDO CONTA VIP: ", end=None)
-                 status = cpm.register_vip(acc2_email, acc2_password)
-                 if status == 0:
-                     print(Colorate.Horizontal(Colors.rainbow, 'SUCESSO'))
-                     print(Colorate.Horizontal(Colors.rainbow, '======================================'))
-                     print(Colorate.Horizontal(Colors.rainbow, 'CONTA VIP CRIADA COM 500.000 GOLDS.'))
-                     sleep(7)
-                     continue
-             elif status == 105:
-                  print(Colorate.Horizontal(Colors.rainbow, 'FALHA.'))
-                  print(Colorate.Horizontal(Colors.rainbow, 'ESSE EMAIL JA EXISTE, TENTE OUTRO.'))
-                  sleep(3)
-                  continue
-             else:
-                  print(Colorate.Horizontal(Colors.rainbow, 'FALHA.'))
-                  print(Colorate.Horizontal(Colors.rainbow, 'TENTE NOVAMENTE.'))
-                  sleep(2)
-                  continue       
-                    
-            else: continue
+              elif service == 27: # Account Register VIP
+                  print(Colorate.Horizontal(Colors.rainbow, '[!] REGISTRANDO CONTA VIP COM 500K GOLDS.'))
+                  acc2_email = prompt_valid_value("[?] INSIRA UM EMAIL", "Email", password=False)
+                  acc2_password = prompt_valid_value("[?] INSIRA UMA SENHA", "Password", password=False)
+                  console.print("[%] CRIANDO CONTA VIP: ", end=None)
+                  status = cpm.register_vip(acc2_email, acc2_password)
+                  if status == 0:
+                      print(Colorate.Horizontal(Colors.rainbow, 'SUCESSO'))
+                      print(Colorate.Horizontal(Colors.rainbow, '======================================'))
+                      print(Colorate.Horizontal(Colors.rainbow, 'CONTA VIP CRIADA COM 500.000 GOLDS.'))
+                      sleep(7)
+                      continue
+              elif status == 105:
+                   print(Colorate.Horizontal(Colors.rainbow, 'FALHA.'))
+                   print(Colorate.Horizontal(Colors.rainbow, 'ESSE EMAIL JA EXISTE, TENTE OUTRO.'))
+                   sleep(3)
+                   continue
+              else:
+                   print(Colorate.Horizontal(Colors.rainbow, 'FALHA.'))
+                   print(Colorate.Horizontal(Colors.rainbow, 'TENTE NOVAMENTE.'))
+                   sleep(2)
+                   continue  
+                   
+              else: continue
             break
         break
