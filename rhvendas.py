64401cb880771b7a0fd2a05139d6eecf31b50f1e
rhvendas <rhuanigorlicurgomuch@gmail.com>
2025-05-17 21:41:46 -0400

    Add files via upload

--- rhvendas.py
+++ rhvendas.py
@@ -222,13 +222,11 @@ class RHvendas:
         response_decoded = response.json()
         return response_decoded.get("ok")
         
-    def login(self, email, password) -> int:
+    def account_register_vip(self, email, password) -> int:
         payload = { "account_email": email, "account_password": password }
         params = { "key": self.access_key }
-        response = requests.post(f"{BASE_URL}/account_login", params=params, data=payload)
+        response = requests.post(f"{BASE_URL}/account_register_vip", params=params, data=payload)
+        response_decoded = response.json()
+        return response_decoded.get("error")    
     
-        print("STATUS CODE:", response.status_code)
-        print("RESPOSTA BRUTA:", response.text)  # Isso mostra o que veio na resposta
-
-        response_decoded = response.json()  # Aqui que quebra
-        return response_decoded.get("error")
+    
